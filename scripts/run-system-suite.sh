d2f81729eeeba1e336d6806e4bb4e7e320bc53ea
mariano.barcia <mariano.barcia@gmail.com>
2026-09-25 19:39:03 +0200

    Disable OTLP export in ordinary RAG system tests

--- scripts/run-system-suite.sh
+++ scripts/run-system-suite.sh
@@ -5,5 +5,6 @@ read -r -a maven_args <<< "${MAVEN_ARGS:-}" || true
 if [[ ${#maven_args[@]} -eq 0 ]]; then
   maven_args=("-Dmaven.repo.local=$PWD/.m2/repository")
 fi
-./mvnw -B verify -Dquarkus.container-image.build=false --no-transfer-progress \
+QUARKUS_OTEL_SDK_DISABLED="${QUARKUS_OTEL_SDK_DISABLED:-true}" \
+  ./mvnw -B verify -Dquarkus.container-image.build=false --no-transfer-progress \
   "${maven_args[@]}"
